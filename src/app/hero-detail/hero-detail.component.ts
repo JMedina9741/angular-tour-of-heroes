d27bcf7435fe9877ec2cb00520de8baaa5a6932e
JMedina9741 <147419480+JMedina9741@users.noreply.github.com>
2023-11-15 19:57:22 +0100

    second-commit

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -14,6 +14,7 @@ import { HeroService } from '../hero.service';
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero | undefined;
+
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
@@ -24,10 +25,16 @@ export class HeroDetailComponent implements OnInit {
   }
   getHero(): void {
     const id =
-    Number(this.route.snapshot.paramMap.get('id'));
+    parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
   goBack(): void {
     this.location.back();
+  }
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
   }
 }
