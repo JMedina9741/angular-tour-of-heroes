0396f262143dd7effd110f96ec8ab55b34d7fe72
JMedina9741 <147419480+JMedina9741@users.noreply.github.com>
2023-11-28 19:19:04 +0100

    third-commit

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -31,18 +31,18 @@ export class HeroService {
   }
 
   /** GET hero by id. Return `undefined` when id not found */
-  getHeroNo404<Data>(id: number): Observable<Hero> {
-    const url = `${this.heroesUrl}/?id=${id}`;
-    return this.http.get<Hero[]>(url)
-      .pipe(
-        map(heroes => heroes[0]), // returns a {0|1} element array
-        tap(h => {
-          const outcome = h ? 'fetched' : 'did not find';
-          this.log(`${outcome} hero id=${id}`);
-        }),
-        catchError(this.handleError<Hero>(`getHero id=${id}`))
-      );
-  }
+  // getHeroNo404<Data>(id: number): Observable<Hero> {
+  //   const url = `${this.heroesUrl}/?id=${id}`;
+  //   return this.http.get<Hero[]>(url)
+  //     .pipe(
+  //       map(heroes => heroes[0]), // returns a {0|1} element array
+  //       tap(h => {
+  //         const outcome = h ? 'fetched' : 'did not find';
+  //         this.log(`${outcome} hero id=${id}`);
+  //       }),
+  //       catchError(this.handleError<Hero>(`getHero id=${id}`))
+  //     );
+  // }
 
   /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
@@ -95,13 +95,13 @@ export class HeroService {
     );
   }
 
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   *
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
+  // /**
+  //  * Handle Http operation that failed.
+  //  * Let the app continue.
+  //  *
+  //  * @param operation - name of the operation that failed
+  //  * @param result - optional value to return as the observable result
+  //  */
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,15 +8,15 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-    { id: 12, name: ' Mr. Incredible' },
-    { id: 13, name: ' Batman' },
-    { id: 14, name: ' V' },
-    { id: 15, name: ' Aquaman' },
-    { id: 16, name: ' Iron Man' },
-    { id: 17, name: ' Ms. Marvel' },
-    { id: 18, name: ' DareDevil' },
-    { id: 19, name: ' Hawkeye' },
-    { id: 20, name: ' Perry the Platypus'}
+    { id: 12, name: 'Mr. Incredible' },
+    { id: 13, name: 'Batman' },
+    { id: 14, name: 'V' },
+    { id: 15, name: 'Aquaman' },
+    { id: 16, name: 'Iron Man' },
+    { id: 17, name: 'Ms. Marvel' },
+    { id: 18, name: 'DareDevil' },
+    { id: 19, name: 'Hawkeye' },
+    { id: 20, name: 'Perry the Platypus'}
   ]; 
     return {heroes};
   }
